5a05a66c1bbd8d6926b1c25227ad6ed3ef97183f
Saad Hussain Syed <90135919+Saad3123@users.noreply.github.com>
2024-07-10 09:30:40 +0000

    added: navbar a,fter -,repositioning and VIU logo--   pending: code cleanup

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,22 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var stickyNav = document.getElementById('stickyNav');
-    var content = document.querySelector('.content');
+// document.addEventListener('DOMContentLoaded', function() {
+//     var stickyNav = document.getElementById('stickyNav');
+//     var content = document.querySelector('.content');
 
-    window.addEventListener('scroll', function() {
-        if (window.scrollY > content.offsetTop) {
-            stickyNav.classList.add('active');
-        } else {
-            stickyNav.classList.remove('active');
-        }
-    });
-});
+//     window.addEventListener('scroll', function() {
+//         if (window.scrollY > content.offsetTop) {
+//             stickyNav.classList.add('active');
+//         } else {
+//             stickyNav.classList.remove('active');
+//         }
+//     });
+// });
+
+window.onscroll = function() {
+    var navbar = document.getElementById("navbar");
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+        navbar.style.top = "0";
+    } else {
+        navbar.style.top = "-60px";
+    }
+};
+
